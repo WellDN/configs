a63d55a1d0622bb1b47e6571b7cf86d4b205cf87
WellDN <Wellingtontp0@gmail.com>
2024-02-27 14:17:19 -0300

    Sign defide for DapBreakpoint

--- nvim/lua/w/remap.lua
+++ nvim/lua/w/remap.lua
@@ -46,3 +46,4 @@ vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", {noremap=
 vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').close({reset = true})<CR>", {noremap=true})
+vim.fn.sign_define('DapBreakpoint', { text='🔴', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
